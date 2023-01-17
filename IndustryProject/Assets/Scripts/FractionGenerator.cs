5dd0c327263331c9c973386c0ed0c761c8efb326
mitchell2001b <mitchell2001b@gmail.com>
2023-01-17 11:36:46 +0100

    updated the fraction scene

--- IndustryProject/Assets/Scripts/FractionGenerator.cs
+++ IndustryProject/Assets/Scripts/FractionGenerator.cs
@@ -12,22 +12,27 @@ public class FractionGenerator : MonoBehaviour
     private List<float> botValues = new List<float>();
 
     [SerializeField] private int botMin = 1;
-    [SerializeField] private int botMax = 8;
+    [SerializeField] private int botMax = 10;
 
-    [SerializeField] private int maxAnswerValue = 20;
+    [SerializeField] private int topMin = 1;
+    [SerializeField] private int topMax = 10;
+
+    [SerializeField] private int maxAnswerValue = 3;
 
     private float answer;
 
     [SerializeField] private TextMeshProUGUI answerTxt;
-    [SerializeField] private Image fractionPie;
+    [SerializeField] private Transform fractionPies;
 
     [SerializeField] private List<GameObject> topButtons;
     [SerializeField] private List<GameObject> botButtons;
 
+    [SerializeField] private List<Image> pies;
+
     [SerializeField] private List<TextMeshProUGUI> topButtonTexts;
     [SerializeField] private List<TextMeshProUGUI> botButtonTexts;
 
-    public float Generate()
+    /*public float Generate()
     {
         botValues.Clear();
         topValues.Clear();
@@ -129,6 +134,100 @@ public class FractionGenerator : MonoBehaviour
         }
 
         fractionPie.rectTransform.localPosition = fractionPieLocation;
+
+        AssignButtons();
+
+        Debug.Log("top row answer index " + topAnswerIndex);
+        Debug.Log("bottom row answer index " + botAnswerIndex);
+
+        return answer;
+    }*/
+
+    public float Generate()
+    {
+        botValues.Clear();
+        topValues.Clear();
+        answer = 0;
+
+        while (topValues.Count < 3)
+        {
+            int rnd = Random.Range(topMin, topMax + 1);
+            if (topValues.Contains(rnd) == false)
+            {
+                topValues.Add(rnd);
+            }
+        }
+
+        int lowestTopValue = topMax + 1;
+        int highestTopValue = 0;
+
+        foreach (int value in topValues)
+        {
+            if (value < lowestTopValue)
+            {
+                lowestTopValue = value;
+            }
+            if (value > highestTopValue)
+            {
+                highestTopValue = value;
+            }
+        }
+
+        while (botValues.Count < 3)
+        {
+            int rnd = Random.Range(botMin, botMax + 1);
+            if (botValues.Contains(rnd) == false && (float)highestTopValue / rnd <= maxAnswerValue)
+            {
+                botValues.Add(rnd);
+            }
+        }
+
+        int topAnswerIndex = Random.Range(0, topValues.Count);
+        int botAnswerIndex = Random.Range(0, botValues.Count);
+
+        answer = (float)(topValues[topAnswerIndex] / botValues[botAnswerIndex]);
+
+        answer = (float)Math.Round(answer, 3);
+
+        answerTxt.text = answer.ToString();
+
+        float piePercent = answer % 1;
+        if (piePercent == 0)
+        {
+            piePercent = 1;
+        }
+        int pieAmount = (int)(answer - piePercent + 1);
+        foreach (Image pie in pies)
+        {
+            pie.fillAmount = 0;
+        }
+        for (int i = 0; i < pieAmount; i++)
+        {
+            pies[i].fillAmount = 1;
+        }
+        pies[pieAmount - 1].fillAmount = piePercent;
+
+        Vector3 fractionPieLocation = fractionPies.localPosition;
+        switch (answer.ToString().Length)
+        {
+            case 1:
+                fractionPieLocation.x = 188;
+                break;
+            /*case 2:
+                fractionPieLocation.x = 243;
+                break;*/
+            case 3:
+                fractionPieLocation.x = 250;
+                break;
+            case 4:
+                fractionPieLocation.x = 290;
+                break;
+            default:
+                fractionPieLocation.x = 330;
+                break;
+        }
+
+        fractionPies.localPosition = fractionPieLocation;
 
         AssignButtons();
 
